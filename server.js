95f664bd9e0e60764e979ddc523944f31a46fd13
ArthurDombroski <tutudombroski@gmail.com>
2025-11-06 19:31:22 -0300

    feat(server): cria rota inicial para resposta padrão do servidor

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const port = 3000;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.status(200).send("Node.js + Express + MongoDB");
+});
+
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
 });
