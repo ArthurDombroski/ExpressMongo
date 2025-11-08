361d43f5047522984578af53ced80f69f23a32b2
ArthurDombroski <tutudombroski@gmail.com>
2025-11-08 12:52:18 -0300

    feat: schemas e models criado

--- src/Models/livros.js
+++ src/Models/livros.js
@@ -1,12 +1,29 @@
-const livros = [
-    {
-        id: 1,
-        titulo: "Senhor dos aneis"
-    },
-    {
-        id: 2,
-        titulo: "Harry Potter"
-    }
-]
+import { ObjectId } from "mongodb";
+import mongoose, { mongo } from "mongoose";
 
-export default livros;
+const livroSchema = new mongoose.Schema({
+    id: {
+        type: ObjectId
+    },
+
+    titulo: {
+        type: String, required: true
+    },
+
+    editora: {
+        type: String
+    },
+
+    preco: {
+        type: Number
+    },
+
+    paginas: {
+        type: Number
+    }
+
+}, {versionKey: false});
+
+const livro = mongoose.model("livros", livroSchema);
+
+export default livro;
