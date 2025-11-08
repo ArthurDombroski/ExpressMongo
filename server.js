38f1bc9c5d2ade34f20a7f7ce10ff2405bb831e6
ArthurDombroski <tutudombroski@gmail.com>
2025-11-08 12:38:04 -0300

    feat: conexão com o banco de dados feita

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import routes from "./src/Routes/livrosRoutes.js"
+import conectaDB from "./src/config/dbConnect.js";
+
+await conectaDB();
+
 
 const app = express();
 const port = 3000;
@@ -12,7 +18,6 @@ app.use(routes);
 app.get("/", (req, res) => {
     res.status(200).send("Node.js + Express + MongoDB");
 });
-
 
 
 app.listen(port, () => {
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,4 +1,19 @@
 import mongoose from "mongoose";
-import 'dotenv/config';
+import dotenv from "dotenv";
+dotenv.config();
+
+async function conectaDB() {
+
+    try {
+        await mongoose.connect(process.env.DATABASE_URL);
+        console.log("Banco de Dados conectado com sucesso");
+
+        return mongoose.connection;
+    } catch(error) {
+        console.log("Falha em conectar o banco de dados", error);
+    }
+}
+
+export default conectaDB;
 
 
